2395460bb71fa9c70d0fe54abc74fc4708a9b6cc
sunil Chauhan <sunilkms@gmail.com>
2017-08-06 01:28:25 +0530

    Create Get-MailboxAllFolderPermission.ps1
    Get Permission for All Mailbox Folder using one Command.
    
    Usage Example:
    
    Get-MailboxAllFolderPermission.ps1 -Mailbox USERID

---
+++ GetMailboxAllFolderPermission/Get-MailboxAllFolderPermission.ps1
@@ -0,0 +1,13 @@
+param ($mailbox)
+
+$AllMbxFolders=Get-MailboxFolderStatistics $mailbox | Select Name, ContainerClass
+$FilteredFolders=$AllMbxFolders | ? { ` # Add or remove the filter for folder in the below list.
+$_.ContainerClass -eq "IPF.Note" -or `
+$_.ContainerClass -like "IPF.Appointment" -and `
+$_.Name -notlike "*Clatter*" -and `
+$_.Name -notlike "*DR*" -and ` 
+$_.Name -notlike "*Junk*" -and `
+$_.Name -notlike "*Sync I*"
+}
+
+foreach ($folder in $FilteredFolders) {Get-MailboxFolderPermission -Identity $($mailbox + ":\$($folder.Name)") -ea SilentlyContinue}
