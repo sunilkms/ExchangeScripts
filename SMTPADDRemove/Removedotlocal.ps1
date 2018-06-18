09dc1d0f82b8eb3b6626272d6fcddeb914b64ff4
sunil Chauhan <sunilkms@gmail.com>
2018-06-18 17:36:10 +0530

    Update Removedotlocal.ps1

--- SMTPADDRemove/Removedotlocal.ps1
+++ SMTPADDRemove/Removedotlocal.ps1
@@ -1,28 +1,17 @@
 param ($file,$add)
-
 $entry = gc $file
-
 foreach ($user in $entry){
-
 $mbx= Get-Mailbox $user
-
 if ($mbx) {
-
-	if ($add) {
-	
+	if ($add) {	
 	Write-host "Adding the ProxyAddress"	
-	$NewProxyAdd=	 
-
+	$NewProxyAdd=
 	} else {
-	Write-host "Removing .local address" $mbx
-	$RemoveSmtp=($mbx.EmailAddresses | ? {$_.smtpAddress -like "*.local"}).SmtpAddress
-	Set-Mailbox $user -EmailAddresses @{remove=$RemoveSmtp}
-	}
-
-	} else {
-
+		Write-host "Removing .local address" $mbx
+		$RemoveSmtp=($mbx.EmailAddresses | ? {$_.smtpAddress -like "*.local"}).SmtpAddress
+		Set-Mailbox $user -EmailAddresses @{remove=$RemoveSmtp}
+	  }
+      } else {
 	Write-host "User Not Found" -f Yellow
-
-  }
-
+      }
 }
