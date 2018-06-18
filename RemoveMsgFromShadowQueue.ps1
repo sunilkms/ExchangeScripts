db2eface5f7c76d828870db9f57d7b71f95f0b45
sunil Chauhan <sunilkms@gmail.com>
2018-06-18 17:50:32 +0530

    Create RemoveMsgFromShadowQueue.ps1

---
+++ RemoveMsgFromShadowQueue.ps1
@@ -0,0 +1,39 @@
+Param(
+[Parameter(Mandatory=$True)]
+[string]$server
+)
+
+#...................................
+# Initialize
+#...................................
+$queues = @()
+$messages = @()
+$currentdate =  (get-date(get-date -Format G)).AddDays(-1)
+
+#...................................
+# Script
+#...................................
+#Get shadow redundancy queues which are on the server
+$queues = get-queue -server $server|where {$_.DeliveryType -eq "ShadowRedundancy" -AND $_.MessageCount -gt "0" }
+
+#Find messages in the shadow redundancy that are older then one day and remove them without confirmation
+Foreach ($queue in $queues)
+        {
+		$messages = @(get-message -queue $queue.Identity|where {$_.DateReceived -lt $currentdate}|select Identity)
+		if ($messages -ne $null){											
+		   			        		Write-Host "Messages found in queue" $queue.identity":" $messages.count -ForegroundColor Red
+							        
+                                    Foreach ($message in $messages) 
+                                               {
+									            Remove-message -identity $message.Identity -WithNDR $false -Confirm:$False
+									           }
+
+								   Write-Host "Cleanup of shadow redundancy queue" $queue.identity "completed" -ForegroundColor Green
+								   Write-Host ""
+								}
+		else{
+			Write-Host "No messages where found to cleanup in shadow redundancy queue" $queue.identity -ForegroundColor Green
+		    Write-Host ""
+			}
+	   }
+							
