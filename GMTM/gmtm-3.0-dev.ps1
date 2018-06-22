3ec5766629bf05e55b9c962ea172cb0144e54785
sunil Chauhan <sunilkms@gmail.com>
2018-06-22 23:30:18 +0530

    Update gmtm-3.0-dev.ps1

--- GMTM/gmtm-3.0-dev.ps1
+++ GMTM/gmtm-3.0-dev.ps1
@@ -5,9 +5,9 @@
 #       . paramiter can be used as switch no need to type True.         
 #Example:
 #       . check how many Mailbox will be moved when you select 200 GB
-#       . GetMbxtoMovefromDB -SizeToMoveInGB 200 -sDB 20-DB44
+#       . GetMbxtoMovefromDB -SizeToMoveInGB 200 -sDB DB44
 #       . Place the Mailbox on Move "-BIL Default value set to 40" to make the pramiter optional
-#       . GetMbxtoMovefromDB -SizeToMoveInGB 200 -sDB 20-DB44 -TDB 20-DB31 -MRS CAS01 -move
+#       . GetMbxtoMovefromDB -SizeToMoveInGB 200 -sDB DB44 -TDB DB31 -MRS CAS01 -move
 #       . Added DB check in advanced.
 #       . added Post Move Size check to prevent the over move size requst.
 #
